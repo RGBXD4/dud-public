0ba99493444ef424507a1e87ef2454f294731621
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-11-10 13:26:23 +0800

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,12 +1,13 @@
 package;
-
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
-
+#end
 using StringTools;
 
 class DiscordClient
 {
+	#if desktop
 	public static var isInitialized:Bool = false;
 	public function new()
 	{
@@ -86,4 +87,5 @@ class DiscordClient
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
+	#end
 }
